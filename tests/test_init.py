addf9777c56b55a18c6e0aebdd7012cac45d4c3c
pablonnaoji <pnnaoji@alumni.berklee.edu>
2023-03-03 19:31:43 -0800

    added more tests

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,3 +1,27 @@
 
-def test_pytest():
+def test_int():
     assert 1 == 1
+
+def test_string():
+    test_string = "Hi"
+    assert "Hi" in test_string
+
+def test_list():
+    test_list = [1,2,3]
+    assert len(test_list) == 3
+
+def test_bool():
+    truthy = True
+    assert truthy == True
+
+def test_even():
+    n = 4
+    assert n % 2 == 0
+
+def test_odd():
+    n = 5
+    assert n % 2 == 1
+
+def test_palindrome():
+    word = "bob"
+    assert word[::-1] == word
